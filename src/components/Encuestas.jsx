090ee990ad487d289911944d56f7f3a03db36192
Alcario <albano_caminos@hotmail.com>
2023-10-02 19:10:22 -0300

    trabajo finalizado

---
+++ src/components/Encuestas.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Encuestas({ encuestas }) {
+  return (
+    <div>
+      <h2>Encuestas Disponibles</h2>
+      <ul>
+        {encuestas.map((encuesta) => (
+          <li key={encuesta.id}>
+            {encuesta.pregunta}
+            <ul>
+              {encuesta.opciones.map((opcion, index) => (
+                <li key={index}>{opcion}</li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Encuestas;
